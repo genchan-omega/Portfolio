27ebabf151eb6ee4196f5f5d0d6b860cbfbaf8ab
genchan_omega <106954010+genchan-omega@users.noreply.github.com>
2025-04-18 20:50:23 +0900

    modify a bit

--- src/components/contact/Contactform/Contactform.tsx
+++ src/components/contact/Contactform/Contactform.tsx
@@ -64,7 +64,7 @@ export default function Contactform() {
           className="w-full h-30 border p-2 rounded"
           required
         />
-        <button type="submit" className="w-full bg-blue-600 hover:bg-blue-800 text-white shadow-md px-4 py-2 rounded transition duration-300">
+        <button type="submit" className="w-full text-xl font-bold border-2 border-cyan-100 hover:bg-gray-700 hover:cursor-pointer shadow-md px-4 py-2 rounded transition duration-300">
           Submit
         </button>
         <p>
